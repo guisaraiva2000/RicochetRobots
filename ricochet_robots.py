c650986ef5ea1f31fb5f08086f2277b99fe97e94
guisaraiva2000 <guilherme.a.saraiva@tecnico.ulisboa.pt>
2020-10-10 18:23:10 +0100

    actions almost done

--- ricochet_robots.py
+++ ricochet_robots.py
@@ -11,7 +11,6 @@ from search import Problem, Node, astar_search, breadth_first_tree_search, \
 import sys
 
 
-
 class RRState:
     state_id = 0
 
@@ -29,60 +28,90 @@ class RRState:
 class Board:
     """ Representacao interna de um tabuleiro de Ricochet Robots. """
 
-    def __init__(self, robot_pos, target_pos, barriers_pos, dimension, barriers_num):
+    def __init__(self, robot_pos, target_pos, target_color, barriers_pos, dimension, barriers_num):
         self.robot_pos = robot_pos
         self.target_pos = target_pos
+        self.target_color = target_color
         self.barriers_pos = barriers_pos
         self.dimension = dimension
         self.barriers_num = barriers_num
 
-
     def robot_position(self, robot: str):
         """ Devolve a posição atual do robô passado como argumento. """
-        # TODO
-        pass
+        return self.robot_pos[robot]
 
-    # TODO: outros metodos da classe
+    def set_robot_position(self, robot: str, new_pos):
+        """ Muda a posição do robô passado como argumento. """
+        self.robot_pos[robot] = new_pos
 
 
 def parse_instance(filename: str) -> Board:
     """ Lê o ficheiro cujo caminho é passado como argumento e retorna
     uma instância da classe Board. """
-    # TODO
+
     f = open(filename, "r")
     dim = int(f.readline())
     file_buf = []
-    barriers = []
-    target = {}
-    dict_pos = {}
+    barriers = {}
+    robot_pos = {}
 
     for i in range(dim):
         file_buf.append(f.readline())
-        dict_pos[file_buf[i][0]] = [int(file_buf[i][2]), int(file_buf[i][4])]
+        robot_pos[file_buf[i][0]] = (int(file_buf[i][2]), int(file_buf[i][4]))
     file_buf.clear()
 
     buf = f.readline()
-    target[buf[0]] = [int(buf[2]), int(buf[4])]
+    target_color = buf[0]
+    target_pos = (int(buf[2]), int(buf[4]))
 
     barriers_number = int(f.readline())
     for i in range(barriers_number):
         file_buf.append(f.readline())
-        barriers.append([int(file_buf[i][0]), int(file_buf[i][2]), file_buf[i][4]])
+        barriers[(int(file_buf[i][0]), int(file_buf[i][2]))] = file_buf[i][4]
 
-    return Board(dict_pos, target, barriers, dim, barriers_number)
+    return Board(robot_pos, target_pos, target_color, barriers, dim, barriers_number)
 
 
 class RicochetRobots(Problem):
-    def __init__(self, board: Board):
+    def __init__(self, board: Board, initial, goal):
         """ O construtor especifica o estado inicial. """
-        # TODO: self.initial = ...
-        pass
+
+        self.initial = initial
+        self.goal = goal
+
+    def actions_aux(self, color: str, state: RRState):
+        possible_individual_actions = []
+
+        pos = state.board.robot_pos[color]
+        line = pos[0]
+        col = pos[1]
+        barriers = state.board.barriers_pos
+        dim = state.board.dimension
+
+        # TODO falta as conficoes de as paredes estarem nas paredes vizinhas
+        if line != 1 and not ((line - 1, col) in barriers and barriers[(line - 1, col)] == 'd'):
+            possible_individual_actions.append((color, 'u'))
+        if line != dim and not ((line + 1, col) in barriers and barriers[(line + 1, col)] == 'u'):
+            possible_individual_actions.append((color, 'd'))
+        if col != dim and not ((line, col + 1) in barriers and barriers[(line, col + 1)] == 'l'):
+            possible_individual_actions.append((color, 'r'))
+        if col != 1 and not ((line, col - 1) in barriers and barriers[(line, col - 1)] == 'r'):
+            possible_individual_actions.append((color, 'l'))
+
+        return possible_individual_actions
 
     def actions(self, state: RRState):
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
-        # TODO
-        pass
+
+        colors = ['G', 'R', 'B', 'Y']
+        possible_actions = []
+
+        for color in colors:
+            possible_actions.extend(self.actions_aux(color, state))
+
+        return possible_actions
+
 
     def result(self, state: RRState, action):
         """ Retorna o estado resultante de executar a 'action' sobre
@@ -111,5 +140,14 @@ if __name__ == "__main__":
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
-    parse_instance(sys.argv[1])
+    # Ler tabuleiro do ficheiro i1.txt:
+    boardd = parse_instance('i1.txt')
+    # Criar uma instância de RicochetRobots:
+    problem = RicochetRobots(boardd, 0, 0)
+    # Criar um estado com a configuração inicial:
+    initial_state = RRState(boardd)
+
+    print(problem.actions(initial_state))
+
+
 
